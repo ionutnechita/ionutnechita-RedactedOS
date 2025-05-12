7b1ad8fc5fcea6bb5ba28d740e0b0476663667e9
Diego Ferrari <dieferhau@me.com>
2025-05-12 00:00:00 +0000

    Called it the [REDACTED] OS

--- kernel/theme/DiegOS.h
+++ kernel/theme/DiegOS.h
@@ -1,4 +1,4 @@
-#define BOOTSCREEN_TEXT "I haven't bothered giving this OS a name yet - %c"
+#define BOOTSCREEN_TEXT "[REDACTED] OS - %c"
 
 #define BOOTSCREEN_NUM_SYMBOLS 13
 #define BOOTSCREEN_NUM_STEPS 12
