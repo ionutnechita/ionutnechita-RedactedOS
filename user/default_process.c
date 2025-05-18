9a9160ee10a49bdfb3b44e218fa8b809fbb23775
Diego Ferrari <dieferhau@me.com>
2025-05-18 00:00:00 +0000

    Restored printf syntax

--- user/default_process.c
+++ user/default_process.c
@@ -4,14 +4,13 @@
 #include "syscalls/syscalls.h"
 
 __attribute__((section(".rodata.proc1")))
-static const char fmt[] = "Process %i";
+static const char fmt[] = "Process %i";//Note: So long as we keep loading this process inside the kernel, we'll need to keep strings outside functions so we can reliably indicate their section
 
 __attribute__((section(".text.proc1")))
 void proc_func() {
     uint64_t j = 0;
     while (1) {
-        printf_args(fmt,&j, 1);
-        j++;
+        printf(fmt, j++);
         for (int i = 0; i < 100000000; i++);
     }
 }
