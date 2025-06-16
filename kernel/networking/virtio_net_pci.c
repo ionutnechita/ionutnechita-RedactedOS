e44305b473002bb84068af3f130ccbdf94c8b03c
Diego Ferrari <dieferhau@me.com>
2025-06-16 00:00:00 +0000

    Printing mac address

--- kernel/networking/virtio_net_pci.c
+++ kernel/networking/virtio_net_pci.c
@@ -29,5 +29,8 @@ bool vnp_find_network(){
         return false;
     }
 
+    uint8_t* mac = vnp_net_dev.device_cfg;
+    kprintf("%x:%x:%x:%x:%x:%x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
     return true;
 }
