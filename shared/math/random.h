37557d91ab12e32dd30914bec7d26c0b0835738c
CodeAnarchist <144830359+CodeAnarchist@users.noreply.github.com>
2025-07-11 22:52:07 +0200

    Create random.h

---
+++ shared/math/random.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+//xoroshiro128+, pseudorandom
+//can be used while waiting for a hardware one
+typedef struct{
+    uint64_t s0;
+    uint64_t s1;
+}rng_t;
+
+static inline uint64_t rotl(uint64_t x, int k){
+    return (x << k)|(x >> (64 - k));
+}
+
+//init
+void rng_seed(rng_t* rng, uint64_t seed);
+
+//single random
+uint8_t  rng_next8(rng_t* rng);
+uint16_t rng_next16(rng_t* rng);
+uint32_t rng_next32(rng_t* rng);
+uint64_t rng_next64(rng_t* rng);
+
+//random in range
+uint8_t  rng_between8(rng_t* rng, uint8_t min, uint8_t max);
+uint16_t rng_between16(rng_t* rng, uint16_t min, uint16_t max);
+uint32_t rng_between32(rng_t* rng, uint32_t min, uint32_t max);
+uint64_t rng_between64(rng_t* rng, uint64_t min, uint64_t max);
+
+//array fill
+void rng_fill8(rng_t* rng, uint8_t* dst, uint32_t count);
+void rng_fill16(rng_t* rng, uint16_t* dst, uint32_t count);
+void rng_fill32(rng_t* rng, uint32_t* dst, uint32_t count);
+void rng_fill64(rng_t* rng, uint64_t* dst, uint32_t count);
+
+#ifdef __cplusplus
+}
+#endif
