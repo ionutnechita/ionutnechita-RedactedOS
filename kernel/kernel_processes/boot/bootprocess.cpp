acd48f3bf2bd0da5e04a696302434c1719669082
Diego Ferrari <dieferhau@me.com>
2025-07-15 00:00:00 +0000

    Made BootSM a pointer

--- kernel/kernel_processes/boot/bootprocess.cpp
+++ kernel/kernel_processes/boot/bootprocess.cpp
@@ -3,15 +3,16 @@
 #include "../kprocess_loader.h"
 #include "console/kio.h"
 
-BootSM state_machine;
+BootSM *state_machine;
 
 extern "C" __attribute__((section(".text.kcoreprocesses"))) void eval_bootscreen() {
     while (1){
-        state_machine.eval_state();
+        state_machine->eval_state();
     }
 }
 
 extern "C" void init_bootprocess() {
+    state_machine = new BootSM();
     create_kernel_process("bootsm",eval_bootscreen);
-    state_machine.initialize();
+    state_machine->initialize();
 }
