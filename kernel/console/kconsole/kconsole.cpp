5bd3dfeb15b3276c44bda163a28fb64301a0e10e
CodeAnarchist <144830359+CodeAnarchist@users.noreply.github.com>
2025-07-25 20:47:43 +0200

    Update kconsole.cpp

--- kernel/console/kconsole/kconsole.cpp
+++ kernel/console/kconsole/kconsole.cpp
@@ -17,9 +17,9 @@ bool KernelConsole::check_ready(){
 }
 
 void KernelConsole::resize(){
-    gpu_size s = gpu_get_screen_size();
-    columns = s.width / char_width;
-    rows = s.height / char_height;
+    gpu_size screen_size = gpu_get_screen_size();
+    columns = screen_size.width / char_width;
+    rows = screen_size.height / char_height;
 
     if (row_data) temp_free(row_data, buffer_data_size);
     buffer_data_size = rows * columns;
@@ -31,7 +31,7 @@ void KernelConsole::resize(){
     }
 
     row_ring.clear();
-    for (uint32_t i=0;i<rows;i++) row_ring.push(i);
+    for (uint32_t i = 0; i < rows; i++) row_ring.push(i);
 }
 
 void KernelConsole::put_char(char c){
@@ -42,33 +42,33 @@ void KernelConsole::put_char(char c){
     }
     if (cursor_x >= columns) newline();
 
-    uint32_t ri;
-    if (row_ring.pop(ri)){
-        row_ring.push(ri);
-        char* line = row_data + ri * columns;
+    uint32_t row_index;
+    if (row_ring.pop(row_index)){
+        row_ring.push(row_index);
+        char* line = row_data + row_index * columns;
         line[cursor_x] = c;
-        gpu_draw_char({cursor_x * char_width,cursor_y*char_height}, c, 1, 0xFFFFFFFF);
+        gpu_draw_char({cursor_x * char_width, cursor_y * char_height}, c, 1, 0xFFFFFFFF);
         cursor_x++;
     }
 }
 
-void KernelConsole::put_string(const char* s){
+void KernelConsole::put_string(const char* str){
     if (!check_ready()) return;
-    for (uint32_t i=0; s[i]; i++) put_char(s[i]);
+    for (uint32_t i = 0; str[i]; i++) put_char(str[i]);
     gpu_flush();
 }
 
-void KernelConsole::put_hex(uint64_t v){
+void KernelConsole::put_hex(uint64_t value){
     if (!check_ready()) return;
     put_char('0');
     put_char('x');
     bool started = false;
-    for(uint32_t i =60 ;; i-=4){
-        uint8_t n = (v>>i)&0xF;
-        char c = n < 10 ? '0' + n : 'A' + (n - 10);
-        if (started || c!='0' || i==0){
-           started = true;
-            put_char(c);
+    for (uint32_t i = 60 ;; i -= 4){
+        uint8_t nibble = (value >> i) & 0xF;
+        char current_char = nibble < 10 ? '0' + nibble : 'A' + (nibble - 10);
+        if (started || current_char != '0' || i == 0){
+            started = true;
+            put_char(current_char);
         }
         if (i == 0) break;
     }
@@ -77,27 +77,27 @@ void KernelConsole::put_hex(uint64_t v){
 
 void KernelConsole::newline(){
     if (!check_ready()) return;
-    uint32_t ri;
-    if (row_ring.pop(ri)){
-        char* line=row_data+ri*columns;
-        for (uint32_t x = cursor_x; x < columns; x++) line[x]=0;
-        row_ring.push(ri);
+    uint32_t row_index;
+    if (row_ring.pop(row_index)){
+        char* line = row_data + row_index * columns;
+        for (uint32_t x = cursor_x; x < columns; x++) line[x] = 0;
+        row_ring.push(row_index);
     }
     cursor_x = 0;
     cursor_y++;
     if (cursor_y >= rows){
         scroll();
-        cursor_y = rows -1;
+        cursor_y = rows - 1;
     }
 }
 
 void KernelConsole::scroll(){
     if (!check_ready()) return;
-    uint32_t ri;
-    if (row_ring.pop(ri)){
-        char* line=row_data+ri*columns;
-        for(uint32_t x = 0; x < columns; x++) line[x]=0;
-        row_ring.push(ri);
+    uint32_t row_index;
+    if (row_ring.pop(row_index)){
+        char* line = row_data + row_index * columns;
+        for (uint32_t x = 0; x < columns; x++) line[x] = 0;
+        row_ring.push(row_index);
     }
     redraw();
 }
@@ -105,10 +105,10 @@ void KernelConsole::scroll(){
 void KernelConsole::redraw(){
     screen_clear();
     for (uint32_t y = 0; y < rows; y++){
-        uint32_t ri;
-        if (row_ring.pop(ri)){
-            row_ring.push(ri);
-            char* line = row_data + ri * columns;
+        uint32_t row_index;
+        if (row_ring.pop(row_index)){
+            row_ring.push(row_index);
+            char* line = row_data + row_index * columns;
             for (uint32_t x = 0; x < columns; x++){
                 gpu_draw_char({x * char_width, y * char_height}, line[x], 1, 0xFFFFFFFF);
             }
@@ -123,12 +123,12 @@ void KernelConsole::screen_clear(){
 void KernelConsole::clear(){
     screen_clear();
     for (uint32_t i = 0; i < rows; i++){
-        uint32_t ri;
-        if (row_ring.pop(ri)){
-            char* line=row_data+ri*columns;
-            for (uint32_t x = 0; x < columns; x++) line[x]=0;
-            row_ring.push(ri);
+        uint32_t row_index;
+        if (row_ring.pop(row_index)){
+            char* line = row_data + row_index * columns;
+            for (uint32_t x = 0; x < columns; x++) line[x] = 0;
+            row_ring.push(row_index);
         }
     }
-    cursor_x=cursor_y = 0;
+    cursor_x = cursor_y = 0;
 }
