065b9d1af3669ed2bcea44cdaed8ff8d525ca257
CodeAnarchist <144830359+CodeAnarchist@users.noreply.github.com>
2025-07-25 17:19:53 +0200

    Update ring_buffer.cpp
    format

--- shared/data_struct/ring_buffer.cpp
+++ shared/data_struct/ring_buffer.cpp
@@ -10,16 +10,18 @@ void cring_init(struct CRingBuffer* rb, void* storage, uint64_t capacity, uint64
     rb->tail = 0;
     rb->full = 0;
 }
+
 uint64_t cring_capacity(const struct CRingBuffer* rb){
     return rb->capacity;
 }
+
 int32_t cring_push(struct CRingBuffer* rb, const void* item){
     if (rb->full) return 0;
 
     uint8_t* base = (uint8_t*)rb->buffer;
     void* dest = base + (rb->head * rb->element_size);
 
-    for(uint32_t i = 0; i < rb->element_size; ++i){
+    for (uint32_t i = 0; i < rb->element_size; ++i){
         ((uint8_t*)dest)[i] =((const uint8_t*)item)[i];
     }
 
@@ -29,17 +31,17 @@ int32_t cring_push(struct CRingBuffer* rb, const void* item){
 }
 
 int32_t cring_pop(struct CRingBuffer* rb, void* out){
-    if(!rb->full && (rb->head == rb->tail)) return 0;
+    if (!rb->full && (rb->head == rb->tail)) return 0;
 
     uint8_t* base = (uint8_t*)rb->buffer;
     void* src = base + (rb->tail * rb->element_size);
 
-    for(uint64_t i = 0; i < rb->element_size; ++i){
+    for (uint64_t i = 0; i < rb->element_size; ++i){
         ((uint8_t*)out)[i] = ((uint8_t*)src)[i];
     }
 
-    rb->tail =(rb->tail + 1) % rb->capacity;
-    rb->full =0;
+    rb->tail = (rb->tail + 1) % rb->capacity;
+    rb->full = 0;
     return 1;
 }
 
